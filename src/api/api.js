fb934bf6fd6d09480bd76b0f07dcc901fce0d4d4
CreteWu <1290599693@qq.com>
2017-07-29 16:06:23 +0800

    api

--- src/api/api.js
+++ src/api/api.js
@@ -6,7 +6,7 @@ import {_webapp} from '../config/_webapp.js'
 const base = 'https://api.duoyunjiav2.wshoto.com';
 const header = 'application/json';
 const dataType = 'json';
-const Promise = require("bluebird");
+// const Promise = require("bluebird");
 /**
  * 分销
  */
@@ -22,7 +22,7 @@ export const commissions = {
   },
 
   /**
-   * 推广订单
+   * 订单统计
    */
   orderStatistics: {
     url: `${base}/commissions/orderStatistics`,
@@ -50,14 +50,24 @@ export const commissions = {
  * @param params
  * @param callback
  */
-function requestweb(params, callback){
-  _webapp.requestx(Object.assign(params, commissions.recordStatistics), res => {
+// function requestweb(params, callback){
+//   _webapp.requestx(Object.assign(params, commissions.recordStatistics), res => {
+//     callback(res)
+//   })
+// }
+
+/**
+ * 佣金统计
+ * @param params
+ * @param callback
+ */
+export const recordStatistics = function(params, callback) {
+  _webapp.requestx(Object.assign(params, commissions.recordStatistics), function(res) {
     callback(res)
   })
-}
-export const recordStatistics = function(params, callback) {
-  console.log(params);
-  _webapp.requestx(Object.assign(params, commissions.recordStatistics), function(res) {
+};
+export const orderStatistics = function(params, callback) {
+  _webapp.requestx(Object.assign(params, commissions.orderStatistics), function(res) {
     callback(res)
   })
 };
