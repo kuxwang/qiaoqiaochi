f096dbc2c539d9b2ecf854858cd664bac425eea8
free-key <18014926353@qq.com>
2017-08-06 22:32:39 +0800

    cxl12345

--- src/components/common/payselect.vue
+++ src/components/common/payselect.vue
@@ -3,7 +3,7 @@
     <section>
       <mt-header title="支付">
         <router-link to="/home" slot="left">
-          <mt-button icon="back">返回</mt-button>
+          <mt-button icon="back"></mt-button>
         </router-link>
       </mt-header>
     </section>
