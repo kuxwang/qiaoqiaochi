8215b6e2e59dc4b9cc7b4abb66f30a6499375c9d
kux <929842746@qq.com>
2017-08-11 16:34:29 +0800

    asd

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,7 @@
  */
 // import {_webapp} from '../config/webapp.js'
 import {_webapp} from '../config/_webapp.js'
-const base = 'https://api.dunoyunjiav2.wshoto.com';
+const base = 'https://api.duoyunjiav2.wshoto.com';
 const header = 'application/json';
 const dataType = 'json';
 
